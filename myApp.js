a79980816d161dbab9fa518a7938e91461dd5bbf
semihorhanbio <50242428+semihorhanbio@users.noreply.github.com>
2023-07-12 10:53:47 +0000

    delete multiple records

--- myApp.js
+++ myApp.js
@@ -70,13 +70,18 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if(err) return done(err); 
+    done(null, data);      
+  })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, data) => {
+      if(err) return done(err); 
+      done(null, data);    
+  })
 };
 
 const queryChain = (done) => {
