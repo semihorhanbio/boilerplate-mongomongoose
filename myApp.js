d9610a2203ae648962d122c9a1a95b4bf98c04d7
semihorhanbio <50242428+semihorhanbio@users.noreply.github.com>
2023-07-12 11:11:43 +0000

    add chain to sort for burrito

--- myApp.js
+++ myApp.js
@@ -86,8 +86,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 'asc'})
+    .limit(2)
+    .select('-age')
+    .exec((err, data) => {
+      if(err) return done(err);
+      done(null, data);
+    })
 };
 
 /** **Well Done !!**
